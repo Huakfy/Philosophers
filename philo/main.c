8a7281211beea840621897ab3435e7dc83b2fedf
Marc JOURNO <mjourno@e2r5p13.clusters.42paris.fr>
2023-02-21 16:43:07 +0100

    init

---
+++ philo/main.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 13:23:02 by mjourno           #+#    #+#             */
+/*   Updated: 2023/02/21 16:18:03 by mjourno          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	not_numeric_chars_only(char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!('0' <= argv[i][j] && argv[i][j] <= '9'))
+				return (1);
+			j++;
+		}
+		if (j == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	long long int	result;
+	int				i;
+
+	i = 0;
+	result = 0;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (i > 10 || result > 2147483647 || result < 0)
+		return (-1);
+	return ((int)result);
+}
+
+int	init_values(t_philo	*philo, char **argv)
+{
+	philo->nb_philo = -1;
+	philo->time_to_die = -1;
+	philo->time_to_eat = -1;
+	philo->time_to_sleep = -1;
+	philo->nb_times_to_eat = -1;
+	philo->nb_philo = ft_atoi(argv[1]);
+	philo->time_to_die = ft_atoi(argv[2]);
+	philo->time_to_eat = ft_atoi(argv[3]);
+	philo->time_to_sleep = ft_atoi(argv[4]);
+	if (philo->nb_philo == -1 || philo->time_to_die == -1
+		|| philo->time_to_eat == -1 || philo->time_to_sleep == -1)
+		return (1);
+	if (argv[5])
+	{
+		philo->nb_times_to_eat = ft_atoi(argv[5]);
+		if (philo->nb_times_to_eat == -1)
+			return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_philo	*philo;
+
+	if (argc < 5 || argc > 6)
+		return (write(2, "Error\nMust have 4 or 5 arguments\n", 33));
+	if (not_numeric_chars_only(argv))
+		return (write(2, "Error\nNon numeric / empty argument\n", 35));
+
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 13:29:06 by mjourno           #+#    #+#             */
+/*   Updated: 2023/02/21 15:40:29 by mjourno          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_philo
+{
+	int	nb_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_times_to_eat;
+}	t_philo;
+
+#endif
